2d4e8ea3f1d2fce8ec226a332788f027a5a8621e
Lokesh Mekala <64378522+lokesh-kumar-m@users.noreply.github.com>
2024-12-15 23:58:52 +0530

    init

---
+++ Kezos/src/main/java/com/app/Kezos/KezosApplication.java
@@ -0,0 +1,13 @@
+package com.app.Kezos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KezosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KezosApplication.class, args);
+	}
+
+}
---
+++ Kezos/src/test/java/com/app/Kezos/KezosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Kezos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KezosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
